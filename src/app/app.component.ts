e2ebaf4ab95b988dbcce1fa70b6b455eb338021c
Marivaldo Sena <msena.ifsp@gmail.com>
2019-05-01 16:09:46 -0300

    Implementar funcionalidade de inserção de atividades.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Todp List';
+  title = 'Todo List';
+  atividades: Array<any> = [];
+  atividade = { nome: '', status: false }
+
+  handleSubmit(form: NgForm) {
+    console.log(form);
+    this.atividades.push({
+      nome: this.atividade.nome,
+      status: this.atividade.status
+    });
+
+    this.atividade.nome = '';
+    this.atividade.status = false;
+  }
 }
